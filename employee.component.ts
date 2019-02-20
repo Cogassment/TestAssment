2de90f46af19b30ef0649955090e6c08b9d6226f
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-02-20 14:45:33 +0530

    Update employee.component.ts

--- employee.component.ts
+++ employee.component.ts
@@ -1,110 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {Allocate} from 'src/app//shared/allocate.model';
-
-
-
-
-@Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
-})
-export class EmployeeComponent implements OnInit {
-formData : Allocate;
-
-textBoxDisabled = true;
-isReadOnly: boolean=true;
-
-    grade =[
-    {id: 1, name:'grade1'},
-    {id: 2, name:'grade2'},
-    {id: 3, name:'grade3'},
-    {id: 4, name:'grade4'}
-      ];
-   billable=[
-        {id:1,name:'yes'},
-        {id:0,name:'No'}
-      ]
-  programme =[
-    {id: 1, name:'programme1'},
-    {id: 2, name:'programme2'},
-    {id: 3, name:'programme3'},
-    {id: 4, name:'programme4'}
-      ];
-  esaproject =[
-    {id: 1, name:'esaproject1'},
-    {id: 2, name:'esaproject2'},
-    {id: 3, name:'esaproject3'},
-    {id: 4, name:'esaproject4'}
-      ];
-   ratecardrole =[
-    {id: 1, name:'ratecardrole1'},
-    {id: 2, name:'ratecardrole2'},
-    {id: 3, name:'ratecardrole3'},
-    {id: 4, name:'ratecardrole4'}
-      ];
-  status =[
-    {id: 1, name:'status1'},
-    {id: 2, name:'status2'},
-    {id: 3, name:'status3'},
-    {id: 4, name:'status4'}
-      ];
-      
-
+export class Allocate {
     
-  constructor( ) { }
-
-  ngOnInit() {
-    this.formData= {
- Cognizant_ID:123456,
- Cognizant_Name:'',
- Cognizant_Grade:0,
- Skill_Sets:'',
- Location:'',
- Cts_Line_Mgr:'',
- ESA_Prj_Name:'',
- Bill_NBill:0,
- Rsrc_Alloc_End_Dt:'',
- VI_End_Dt_Onshore:'',
- VI_Type:'',
- VI_Ext_Posbl_Bynd_Enddt:0,
- VI_Hw_Lng_Can_Ext:'',
- VI_Ext_Posble_Frm_UK:0,
- Resource_Name:'',
- Rate_Card_Role:0,
- Rate_Card_Day_Rate:'',
- On_Off_2017:'',
- Project_Programme:0,
- Portfolio_2017:'',
- Old_SOW:'',
- New_SOW_End_date:'',
- New_SOW:'',
- Billable:0,
- RLG_Account_Y_N:0,
- RLG_StaffID:0,
- CTS_EL:'',
- RLG_ID:0,
- RLG_Email:'',
- RLG_Join_Date:'',
- RLG_Leaving_Date:'',
- RLG_Rprt_Mgr:'',
- Asset_No_Onshore:'',
- VM_No_Offshore:'',
- Comments:'',
- Primary_Skill:'',
- status:''
-  }
-    
+Cognizant_ID:number;
+Cognizant_Name:string;
+Cognizant_Grade:number;
+Skill_Sets:string;  
+Location:string;
+Cts_Line_Mgr:string;
+ESA_Prj_Name:string;
+Bill_NBill:number;
+Rsrc_Alloc_End_Dt:string;
+VI_End_Dt_Onshore:string;
+VI_Type:string;
+VI_Ext_Posbl_Bynd_Enddt:number;
+VI_Hw_Lng_Can_Ext:string;
+VI_Ext_Posble_Frm_UK:number;
+Resource_Name:string;
+Rate_Card_Role:number;
+Rate_Card_Day_Rate:string;
+On_Off_2017:string;
+Project_Programme:number;
+Portfolio_2017:string;
+Old_SOW:string;
+New_SOW_End_date:string;
+New_SOW:string;
+Billable:number;
+RLG_Account_Y_N:number;
+RLG_StaffID:number;
+CTS_EL:string;
+RLG_ID:number;
+RLG_Email:string;
+RLG_Join_Date:string;
+RLG_Leaving_Date:string;
+RLG_Rprt_Mgr:string;
+Asset_No_Onshore:string;
+VM_No_Offshore:string;
+Comments:string;
+Primary_Skill:string;
+status:string;
 }
-
-
-toggle(){
-    this.textBoxDisabled = !this.textBoxDisabled;
-  }
-
-onSubmit(form : NgForm){
-alert(this.formData.RLG_Account_Y_N +' '+this.formData.Billable);
-}
-}
-
